c57c4980ac6480d06425837613a8de4438438323
iyango <iyango2000@gmail.com>
2019-04-22 14:58:52 -0400

    Add files via upload

---
+++ OddEvenNum Practice/OddEvenNum Practice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OddEvenNum_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a Number: ");
+            string useinput;
+            useinput = Console.ReadLine();
+            int changed = int.Parse(useinput);
+
+
+            if (changed % 2 == 0)
+            {
+                Console.Write(" Entered Number is an Even Number");
+
+
+            }
+            else
+
+                Console.Write(" Eneterd Number is an Odd Number");
+            Console.ReadLine();
+        }
+    }
+}
